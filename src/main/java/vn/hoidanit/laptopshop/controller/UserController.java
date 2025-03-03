1f4e977f8ad45a811a4a695d51e890ac8b6df0e4
LamVanChuong04 <lamvanchuong04@gmail.com>
2025-03-03 11:32:47 +0700

    day 2

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,11 +1,13 @@
 package vn.hoidanit.laptopshop.controller;
 
-//import vn.hoidanit.laptopshop.service.UserService;
+import vn.hoidanit.laptopshop.service.UserService;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 //import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 
 // @RestController
@@ -23,13 +25,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
-    // private UserService userService;
+    private UserService userService;
     
-    // public UserController(UserService userService){
-    //     this.userService = userService;
-    // }
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
     @RequestMapping("/")
-    public String  getHomePage(){
-        return "home.html";
+    public String  getHomePage(Model model){
+        String test = this.userService.getHomePage();
+        model.addAttribute("test", test);
+        return "hello";
+    }
+    @RequestMapping("/admin/user")
+    public String  getUserPage(Model model){
+        String test = this.userService.getHomePage();
+        
+        return "admin/user/create";
+    }
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String  createUser(Model model){
+        
+        return "hello";
     }
 }
---
+++ src/main/webapp/WEB-INF/view/hello.jsp
@@ -0,0 +1,33 @@
+<%@page contentType="text/html" pageEncoding="UTF-8" %>
+    <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+        <!DOCTYPE html>
+
+        <html>
+
+        <head>
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <title></title>
+            <meta name="description" content="">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+            <!-- Latest compiled JavaScript -->
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+            <link rel="stylesheet" href="/css/demo.css">
+        </head>
+
+        <body>
+
+            <h1>hello lam van chuong</h1>
+
+            <h2> ${test}</h2>
+
+            <button> SUBMIT</button>
+            <script src="" async defer></script>
+        </body>
+
+        </html>
